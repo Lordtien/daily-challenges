5a4f92962e47bc229828390430fde2341b1ba1b7
Lordtien <nitinpawarme@gmail.com>
2020-01-07 18:04:01 +0530

    Solve pascals-triangle

---
+++ jan-6/pascals-triangle/pascals-triangle.test.js
@@ -0,0 +1,8 @@
+import generate from './pascals-triangle';
+
+describe('Should pass all the tests', () => {
+  test('Should merge sorted arrays correctly', () => {
+    expect(generate(5)).toEqual([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]);
+    expect(generate(0)).toEqual([]);
+  });
+});
