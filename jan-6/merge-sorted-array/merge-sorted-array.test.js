0100255c4d31538b8d3833e03ca25374e5e06d2b
Lordtien <nitinpawarme@gmail.com>
2020-01-07 18:02:24 +0530

    Solve merge-sorted-array

--- jan-6/merge-sorted-array/merge-sorted-array.test.js
+++ jan-6/merge-sorted-array/merge-sorted-array.test.js
@@ -2,8 +2,17 @@ import merge from './merge-sorted-array';
 
 describe('Should pass all the tests', () => {
   test('Should merge sorted arrays correctly', () => {
-    expect(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)).toBe([1, 2, 2, 3, 5, 6]);
-    expect(merge([2, 0], 1, [1], 1)).toBe([1, 2]);
-    expect(merge([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3)).toBe([-1, 0, 0, 1, 2, 2, 3, 3, 3]);
+    let nums1 = [1, 2, 3, 0, 0, 0];
+    let nums2 = [2, 5, 6];
+    merge(nums1, 3, nums2, 3);
+    expect(nums1).toStrictEqual([1, 2, 2, 3, 5, 6]);
+    nums1 = [2, 0];
+    nums2 = [1];
+    merge(nums1, 1, nums2, 1);
+    expect(nums1).toStrictEqual([1, 2]);
+    nums1 = [-1, 0, 0, 3, 3, 3, 0, 0, 0];
+    nums2 = [1, 2, 2];
+    merge(nums1, 6, nums2, 3);
+    expect(nums1).toStrictEqual([-1, 0, 0, 1, 2, 2, 3, 3, 3]);
   });
 });
